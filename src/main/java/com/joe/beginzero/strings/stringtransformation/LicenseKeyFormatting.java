84d1848bcf90d4d23f06c028b0a3c03ecc2d7a8e
joe <kaihua.ckh@gmail.com>
2020-09-13 22:05:18 +0800

    leetcode

---
+++ src/main/java/com/joe/beginzero/strings/stringtransformation/LicenseKeyFormatting.java
@@ -0,0 +1,50 @@
+package com.joe.beginzero.strings.stringtransformation;
+
+/**
+ * 482. License Key Formatting
+ *
+ * @author ckh
+ * @create 9/13/20 7:59 PM
+ */
+public class LicenseKeyFormatting {
+
+    public static String licenseKeyFormatting(String S, int K) {
+        char[] chars = S.toCharArray();
+        char[] result = new char[chars.length + S.length() / K];
+        int length = 0;
+        // 逆序便利
+        int i = chars.length - 1, j = result.length - 1;
+        while (i >= 0) {
+            if (chars[i] == '-') {
+                i--;
+                continue;
+            }
+            if (chars[i] >= 'a' && chars[i] <= 'z') {
+                chars[i] = (char) (chars[i] - 32);
+            }
+            if (length != K) {
+                result[j] = chars[i];
+                length++;
+                j--;
+                if (i == 0) {
+                    break;
+                } else {
+                    i--;
+                }
+            } else {
+                length = 1;
+                result[j] = '-';
+                result[j - 1] = chars[i];
+                j -= 2;
+                i--;
+            }
+        }
+
+        return new String(result, j + 1, result.length - j - 1);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(licenseKeyFormatting("5F3Z-2e-9-w", 4));
+    }
+}
--- src/test/java/cn/TestCsApp.java
+++ src/test/java/cn/TestCsApp.java
@@ -7,12 +7,21 @@ import org.junit.Test;
  * @create 2020/8/11 10:33
  */
 public class TestCsApp {
- 
+
     @Test
-    public void testbit(){
-        System.out.println(Integer.toBinaryString(-121));
-        System.out.println(Integer.toBinaryString(-121 << 4));
-        System.out.println(Integer.toBinaryString(-121 >> 4));
-        System.out.println(Integer.toBinaryString(-121 >>> 4));
+    public void testBit() {
+        int num = -0b10010101;
+        System.out.println(55);
+        System.out.println(Integer.toBinaryString(55));
+        System.out.println(Integer.toBinaryString(num << 4));
+        System.out.println(Integer.toBinaryString(num >> 4));
+        System.out.println(Integer.toBinaryString(num >>> 4));
+
+
+        int x = 0xffff;
+        System.out.println(x * x);
+
+        // 补码乘法， 乘以常数 ！！！
+        System.out.println((0xff << 8) - 0xff);
     }
 }
