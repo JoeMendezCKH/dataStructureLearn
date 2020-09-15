122d53cf83e380cc7a75d64ad8a27fb5d1e8839f
joe <kaihua.ckh@gmail.com>
2020-09-15 21:29:17 +0800

    leetcode

--- src/main/java/com/joe/beginzero/strings/stringtransformation/ZigZagConversion.java
+++ src/main/java/com/joe/beginzero/strings/stringtransformation/ZigZagConversion.java
@@ -1,28 +1,44 @@
 package com.joe.beginzero.strings.stringtransformation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * 6. ZigZag Conversion
+ *
  * @author ckh
  * @create 9/13/20 8:39 PM
  */
 public class ZigZagConversion {
 
-    public String convert(String s,int numRows){
-        if (numRows == 1) {
+    /**
+     * https://leetcode-cn.com/problems/zigzag-conversion/solution/zzi-xing-bian-huan-by-jyd/
+     */
+    public static String convert(String s, int numRows) {
+        if (numRows < 2) {
             return s;
         }
-        StringBuilder sb = new StringBuilder();
-        int n = s.length();
+        List<StringBuilder> rows = new ArrayList<>();
+
         for (int i = 0; i < numRows; i++) {
-            for (int j = i; j < n; j += (numRows - 1) * 2) {
-                sb.append(s.charAt(j));
-                if (i > 0 && i < numRows - 1) {
-                    int sec = j + 2 * (numRows - i - 1);
-                    if (sec < n) {
-                        sb.append(s.charAt(sec));
-                    }
-                }
-            }
+            rows.add(new StringBuilder());
         }
-        return sb.toString();
+        int i = 0, flag = -1;
+        for (char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if (i == 0 || i == numRows - 1) {
+                flag = -flag;
+            }
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder row : rows) {
+            res.append(row);
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert("leetcode", 3));
     }
 }
